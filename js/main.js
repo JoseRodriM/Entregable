e5b006815b68a5671e1f1ca8492868cbfc0c6483
JoseRodriM <josearm_1601@hotmail.com>
2022-02-07 17:26:40 +0100

    Primer avance

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let navMenu = document.getElementById('nav-list'),
+    navToggle = document.getElementById('nav-menu'),
+    navClose = document.getElementById('nav-close');
+
+
+if (navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
